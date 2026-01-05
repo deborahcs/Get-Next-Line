0d9098bbbb6c32ad91d7a8c1dac7f5491d31ddf6
decabral <deborahcabralsilva@gmail.com>
2026-01-05 15:21:50 -0300

    Header

--- get_next_line.h
+++ get_next_line.h
@@ -6,11 +6,11 @@
 # include <unistd.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 10
+#  define BUFFER_SIZE 42
 # endif
 
 char	*get_next_line(int fd);
-char	*ft_strjoin(char *s1, char const *s2);
+char	*ft_strjoin(char *s1, char *s2);
 char	*ft_strchr(const char *s, int c);
 size_t	ft_strlen(const char *s);
 
